e2b862f1b8956d17217851c3bd4ef0ce0b12def3
edisonbedoya98 <edison.bedoyag@udea.edu.co>
2023-02-18 12:23:47 -0500

    Now most of the test already passed, now there are two left, which are intruct robot and instruct many robots

--- ubirobot/robot-simulator.js
+++ ubirobot/robot-simulator.js
@@ -8,6 +8,7 @@ export class InvalidInputError extends Error {
 export class Robot {
   constructor() {
     this.allowedDirecions = ["east", "west", "north", "south"];
+    this.robotPosition = [0, 0];
   }
 
   orient(direction) {
@@ -25,16 +26,23 @@ export class Robot {
   }
 
   get coordinates() {
-    throw new Error("Remove this statement and implement this function");
+    return this.robotPosition;
+    // throw new Error("Remove this statement and implement this function");
   }
 
   turnRight() {
     switch (this.robotBearing) {
       case "north":
-        //this.
+        this.robotBearing = "east";
         break;
-
-      default:
+      case "east":
+        this.robotBearing = "south";
+        break;
+      case "south":
+        this.robotBearing = "west";
+        break;
+      case "west":
+        this.robotBearing = "north";
         break;
     }
 
@@ -42,26 +50,79 @@ export class Robot {
   }
 
   turnLeft() {
-    throw new Error("Remove this statement and implement this function");
+    switch (this.robotBearing) {
+      case "north":
+        this.robotBearing = "west";
+        break;
+      case "east":
+        this.robotBearing = "north";
+        break;
+      case "south":
+        this.robotBearing = "east";
+        break;
+      case "west":
+        this.robotBearing = "south";
+        break;
+    }
+
+    // throw new Error("Remove this statement and implement this function");
   }
 
-  at() {
-    throw new Error("Remove this statement and implement this function");
+  at(x, y) {
+    this.robotPosition = [x, y];
+    // throw new Error("Remove this statement and implement this function");
   }
 
   advance() {
-    throw new Error("Remove this statement and implement this function");
+    const x = this.robotPosition[0];
+    const y = this.robotPosition[1];
+
+    switch (this.robotBearing) {
+      case "north":
+        this.robotPosition = [x, y + 1];
+        break;
+      case "east":
+        this.robotPosition = [x + 1, y];
+        break;
+      case "south":
+        this.robotPosition = [x, y - 1];
+        break;
+      case "west":
+        this.robotPosition = [x - 1, y];
+        break;
+    }
+    // new Error("Remove this statement and implement this function");
   }
 
-  instructions() {
-    throw new Error("Remove this statement and implement this function");
+  static instructions(instructions) {
+    const instructionsArray = instructions.split("");
+    const decodedInstructions = [];
+
+    for (let index = 0; index < instructionsArray.length; index++) {
+      switch (instructionsArray[index]) {
+        case "L":
+          decodedInstructions.push("turnLeft");
+          break;
+        case "R":
+          decodedInstructions.push("turnRight");
+          break;
+        case "A":
+          decodedInstructions.push("advance");
+          break;
+      }
+    }
+    return decodedInstructions;
+    // throw new Error("Remove this statement and implement this function");
   }
 
-  place() {
-    throw new Error("Remove this statement and implement this function");
+  place({ x, y, direction }) {
+    this.robotPosition = [x, y];
+    this.robotBearing = direction;
+    //throw new Error("Remove this statement and implement this function");
   }
 
-  evaluate() {
-    throw new Error("Remove this statement and implement this function");
+  evaluate(instructions) {
+    //instructions(instructions);
+    //throw new Error("Remove this statement and implement this function");
   }
 }
